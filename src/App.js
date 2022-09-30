427db9545a9c4db397b698627bc85910ed9c5f55
Carlos Ferraz <carloslf@e.email>
2022-09-30 14:30:37 +0300

    Hardcoding test of map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import MyMap from './component/map';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import * as restaurant from './component/restaurantData.json';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <MyMap />
-    </div>
+    <MapContainer center={[49.420429, 11.700490]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                
+      />
+
+      {/* {restaurant.map() */}
+      <Marker key={restaurant} position={[
+        restaurant?.latitude,
+        restaurant?.longetude
+      ]}
+      />
+    </MapContainer>
   );
 }
 
-export default App;
+
--- src/component/map.js
+++ src/component/map.js
@@ -1,22 +1,9 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 function MyMap() {
 
-    const position = [1.35, 103.8]
 
-    return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>,
-    )
+    return 
 }
 
 export default MyMap;
